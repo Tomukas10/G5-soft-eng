5a71a90ff547df4c95f9b4661a9df3efb48875b7
mv2025 <144661539+mv2025@users.noreply.github.com>
2025-03-06 22:22:36 +0000

    Update authRoutes.js

--- JS/authRoutes.js
+++ JS/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const db = require("../config/db");
+const db = require("../db");
 
 const router = express.Router();
 
